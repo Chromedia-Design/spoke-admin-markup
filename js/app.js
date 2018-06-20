8e2cfc14e0af3464889aa4c483a7be6121822ed1
Arthur Baduyen <arthurbaduyen@Arthur.local>
2018-06-20 20:38:10 +0800

    compact message notification

--- js/app.js
+++ js/app.js
@@ -1,26 +1,41 @@
 $(document).foundation();
 
-$(".chat-button-container").on("click",function(){
-  $(this).siblings('.chat-container').slideToggle()
+$("#chat-toggle").on("click",function(){
+  $("#chat-notification").slideUp()
+  $(this).siblings('#chat-container').slideToggle()
 })
 
-$(".chat-header-container").on("click",function(event){
+$("#chat-container .chat-header-container").on("click",function(event){
   console.log("in")
   if( ($(event.target).hasClass('btnBack')) || ($(event.target).hasClass('fa')) ) {
     $(this).children('.chat-header').addClass('shrinked')
-    $(this).siblings('.chat-header-container').slideDown()
-    $(this).siblings('.chat-body-container, .chat-input-container').slideDown()
-    $(this).siblings('.chat-header-intro').show()
+    $(this).prevAll('.chat-header-container').slideToggle()
+    $(this).nextAll('.chat-header-container').slideToggle()
+    $(this).siblings('.chat-header-intro').slideToggle()
+    $(this).siblings('.chat-body-container, .chat-input-container').addClass('hide')
+    // $(this).siblings('.chat-header-intro').show()
     return;
   }
 
   
-  $(this).siblings('.chat-header-container, .chat-header-intro').slideUp()
-  // $(this).nextAll('.chat-header-container').slideUp()
+  $(this).siblings('.chat-header-container').slideUp()
   $(this).siblings('.chat-body-container, .chat-input-container').removeClass('hide')
   $(this).children('.chat-header').removeClass('shrinked active')
-  // $(this).siblings('.chat-header-intro').addClass('hide')
+  $(this).siblings('.chat-header-intro').slideUp()
 })
+
+$("#chat-notification").on("click",".chat-header-container",function(event){
+  openNotification()  
+})
+
+function openNotification() {
+  $("#chat-notification").slideUp()
+  $("#chat-container").slideToggle()
+  $("#chat-container").children('.chat-header-intro').hide()
+  $("#chat-container").children('.chat-header-container:first').children(".chat-header").removeClass('shrinked active')
+  $("#chat-container").children('.chat-header-container:first').siblings(".chat-header-container").hide()
+  $("#chat-container").children('.chat-header-container:first').siblings(".chat-body-container, .chat-input-container").removeClass('hide')
+}
 
 // replace these values with those generated in your TokBox Account
 var apiKey = "45907482";
