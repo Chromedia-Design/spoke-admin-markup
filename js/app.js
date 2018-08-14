1e3537d5b8a9ffc2ee972ba62e195c4a737f08d5
Arthur Baduyen <arthurbaduyen@Arthur.local>
2018-08-14 20:36:40 +0800

    fix js

--- js/app.js
+++ js/app.js
@@ -82,21 +82,20 @@ $("#btnAttach").on("click", function () {
 })
 
 // Send Message
-<<<<<<< HEAD
 $("#btnSend").on("click", function(){
-console.log("Send Message")
-$(this).parents(".chat-input-container").animate({"height": "63px"}, 400)
-$(this).parent(".chat-input").prev(".attachments").slideUp()
-$(this).parents(".chat-input-container").siblings(".chat-body-container").slideDown()
-$(this).parents(".chat-input-container").siblings(".chat-body-container").animate({"height": "406px"}, 400)
-=======
+  console.log("Send Message")
+  $(this).parents(".chat-input-container").animate({"height": "63px"}, 400)
+  $(this).parent(".chat-input").prev(".attachments").slideUp()
+  $(this).parents(".chat-input-container").siblings(".chat-body-container").slideDown()
+  $(this).parents(".chat-input-container").siblings(".chat-body-container").animate({"height": "406px"}, 400)
+})
+
 $("#btnSend").on("click", function () {
     console.log("Send Message")
     $(this).parents(".chat-input-container").animate({"height": "63px"}, 400)
     $(this).parent(".chat-input").prev(".attachments").slideUp()
     $(this).parents(".chat-input-container").siblings(".chat-body-container").slideDown()
     $(this).parents(".chat-input-container").siblings(".chat-body-container").animate({"height": "456px"}, 400)
->>>>>>> e5ea3c478440e8b7fa841b620451ddc90cc9bf0f
 })
 
 // Answer Video Call
