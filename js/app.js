ef3f3b4e06748131c65c498ab8fcd628132234f5
Arthur Baduyen <arthur.baduyen@chromedia.com>
2018-10-02 14:41:40 +0800

    builder: question

--- js/app.js
+++ js/app.js
@@ -230,16 +230,16 @@ $(".clinical-pathway-new-entry-list").on('click', '.clinical-pathway-new-entry',
 
 
 // TO BE REMOVED ------------------------------------------------------------
-$(".toggle-sibling").children(":nth-child(1)").on('click', function(event) {
-  event.preventDefault();
-  $(this).siblings().slideToggle()
-});
-$(".toggle-sibling").children(":nth-child(2)").slideToggle();
+// $(".toggle-sibling").children(":nth-child(1)").on('click', function(event) {
+//   event.preventDefault();
+//   $(this).siblings().slideToggle()
+// });
+// $(".toggle-sibling").children(":nth-child(2)").slideToggle();
 // TO BE REMOVED ------------------------------------------------------------
 
 
 
-// TOGGLER, toggle second child by clicking the first child
+// TOGGLER: toggle "toggler-target" by clicking "toggler-handler"
 $(".toggler").find(".toggler-target").slideUp()
 
 // toggle target
@@ -251,6 +251,18 @@ $(".toggler").on('click', '.toggler-handler', function(event) {
   console.log(event.target.nodeName)
 });
 
+function selectOnClick(target,targetTree, selectionClass){
+    $(target).on('click', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        $(this).parents(targetTree).find(target).removeClass(selectionClass)
+        $(this).addClass(selectionClass)
+        
+        // $(this).siblings(target).removeClass(selectionClass)
+    });
+}
+
+selectOnClick(".ul-tree-child",".ul-tree","selected")
 
 // replace these values with those generated in your TokBox Account
 var apiKey = "45907482";
